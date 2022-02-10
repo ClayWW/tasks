a57f1fd95ad9c854a80ba15f9b50049e5f5847d7
Clay Wilfong <claywilfong@clays-mbp.myfiosgateway.com>
2022-02-10 13:56:18 -0500

    Hello World Added

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App(): JSX.Element {
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Clay Wilfong has edited successfully.
+                automatically reload. Clay Wilfong has edited successfully. Hello World.
             </p>
         </div>
     );
