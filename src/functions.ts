70a86583688bfa1f725e6da79065e1ef2e2b2905
Clay Wilfong <claywilfong@clays-mbp.myfiosgateway.com>
2022-02-20 18:03:33 -0500

    Added working code for the function problems provided

--- src/functions.ts
+++ src/functions.ts
@@ -4,7 +4,7 @@
  *      C = (F - 32) * 5/9
  */
 export function fahrenheitToCelius(temperature: number): number {
-    return 0;
+    return (temperature - 32) * (5 / 9);
 }
 
 /**
@@ -12,7 +12,17 @@ export function fahrenheitToCelius(temperature: number): number {
  * if the number is greater than zero.
  */
 export function add3(first: number, second: number, third: number): number {
-    return 0;
+    let result = 0;
+    if (first > 0) {
+        result = result + first;
+    }
+    if (second > 0) {
+        result = result + second;
+    }
+    if (third > 0) {
+        result = result + third;
+    }
+    return result;
 }
 
 /**
@@ -20,7 +30,7 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    return message.toUpperCase() + "!";
 }
 
 /**
@@ -28,7 +38,7 @@ export function shout(message: string): string {
  * mark. Do not use an `if` statement in solving this question.
  */
 export function isQuestion(message: string): boolean {
-    return true;
+    return message.charAt(message.length - 1) === "?" ? true : false;
 }
 
 /**
@@ -37,5 +47,13 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    let indicator = null;
+    if (word === "yes" || word === "YES") {
+        indicator = true;
+    } else if (word === "no" || word === "NO") {
+        indicator = false;
+    } else {
+        indicator = null;
+    }
+    return indicator;
 }
